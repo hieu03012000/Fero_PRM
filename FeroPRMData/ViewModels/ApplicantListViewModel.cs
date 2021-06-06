5e8a370ee05b29915be6297efcf04403fa617f8c
hieu03012000 <hieu03012000@gmail.com>
2021-06-06 16:53:17 +0700

    view model detail and get all style

--- FeroPRMData/ViewModels/ApplicantListViewModel.cs
+++ FeroPRMData/ViewModels/ApplicantListViewModel.cs
@@ -6,13 +6,8 @@ namespace Fero.Data.ViewModels
 {
     public class ApplicantListViewModel
     {
-        public ApplicantListViewModel()
-        {
-            BodyPart = new HashSet<ModelDetailBodyPartViewModel>();
-        }
         public string Name { get; set; }
         public byte Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public virtual ICollection<ModelDetailBodyPartViewModel> BodyPart { get; set; }
     }
 }
--- FeroPRMData/ViewModels/ModelDetailViewModel.cs
+++ FeroPRMData/ViewModels/ModelDetailViewModel.cs
@@ -7,71 +7,36 @@ namespace Fero.Data.ViewModels
     {
         public ModelDetailViewModel()
         {
-            BodyPart = new HashSet<ModelDetailBodyPartViewModel>();
+            Image = new HashSet<ModelDetailImageViewModel>();
             ModelStyle = new HashSet<ModelDetailModelStyleViewModel>();
-            Product = new HashSet<ModelDetailProductViewModel>();
         }
+        public string Id { get; set; }
         public string Name { get; set; }
-        public byte Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string Country { get; set; }
-        public string SubAddress { get; set; }
+        public string Address { get; set; }
+        public int? Gender { get; set; }
+        public decimal? Height { get; set; }
+        public decimal? Weight { get; set; }
+        public decimal? Bust { get; set; }
+        public decimal? Waist { get; set; }
+        public decimal? Hip { get; set; }
+        public string SocialNetworkLink { get; set; }
         public string Phone { get; set; }
-        public string Email { get; set; }
-        public string Gifted { get; set; }
-        public virtual ICollection<ModelDetailBodyPartViewModel> BodyPart { get; set; }
+        public string Avatar { get; set; }
+        public string Gmail { get; set; }
+
+        public virtual ICollection<ModelDetailImageViewModel> Image { get; set; }
         public virtual ICollection<ModelDetailModelStyleViewModel> ModelStyle { get; set; }
-        public virtual ICollection<ModelDetailProductViewModel> Product { get; set; }
     }
-
-    public class ModelDetailBodyPartViewModel
-    {
-        public ModelDetailBodyPartViewModel()
-        {
-            BodyAttribute = new HashSet<ModelDetailBodyAttributeViewModel>();
-            CollectionImage = new HashSet<ModelDetailCollectionImageViewModel>();
-        }
-        public int? BodyPartTypeId { get; set; }
-        public string BodyPartTypeName { get; set; }
-        public virtual ICollection<ModelDetailBodyAttributeViewModel> BodyAttribute { get; set; }
-        public virtual ICollection<ModelDetailCollectionImageViewModel> CollectionImage { get; set; }
-    }
-
-    public class ModelDetailBodyAttributeViewModel
-    {
-        public decimal? Value { get; set; }
-        public int? BodyAttTypeId { get; set; }
-        public string BodyAttName { get; set; }
-        public string Measure { get; set; }
-    }
-
-    public partial class ModelDetailProductViewModel
-    {
-        public string Link { get; set; }
-    }
-
+   
     public partial class ModelDetailModelStyleViewModel
     {
         public int StyleId { get; set; }
         public string StyleName { get; set; }
     }
 
-    public partial class ModelDetailCollectionImageViewModel
-    {
-        public ModelDetailCollectionImageViewModel()
-        {
-            Image = new HashSet<ModelDetailImageViewModel>();
-        }
-
-        public string Name { get; set; }
-        public virtual ICollection<ModelDetailImageViewModel> Image { get; set; }
-    }
-
     public partial class ModelDetailImageViewModel
     {
-        public string Extension { get; set; }
-        public string FileName { get; set; }
-        public DateTime UploadDate { get; set; }
+        public string Link { get; set; }
 
     }
 }
